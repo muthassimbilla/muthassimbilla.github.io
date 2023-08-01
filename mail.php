991abb95165752327d7386b21f75f21319cb53e7
muthassimbilla <122305833+muthassimbilla@users.noreply.github.com>
2023-08-01 15:41:59 +0000

    main

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
 
-$recepient = "your.mail@here.com";
-$sitename = "Arter";
+$recepient = "muthassimbilla4@gmail.com";
+$sitename = "Billa";
 
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
